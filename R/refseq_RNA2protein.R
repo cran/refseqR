4c510ce9157a18d05299800a743e69994a1eb69d
Jose V. Die <jose.die@uco.es>
2024-10-30 00:50:01 +0000

    version 1.1.5

--- R/refseq_RNA2protein.R
+++ R/refseq_RNA2protein.R
@@ -14,11 +14,11 @@
 #' @seealso \code{\link{refseq_protein2RNA}} to obtain the transcript ids that encode a set of proteins ids.
 #'
 #' @examples
+#'  \dontrun{
 #'  # Get the protein id from a single transcript accession
 #'  transcript <- "XM_004487701"
 #'  refseq_RNA2protein(transcript)
 #'
-#'  \donttest{
 #'  # Get the protein ids from a set of transcript accessions
 #'  transcript = c("XM_004487701", "XM_004488493")
 #'  sapply(transcript, function(x) refseq_RNA2protein(x), USE.NAMES = FALSE) }
@@ -41,7 +41,7 @@ refseq_RNA2protein <- function(transcript) {
       return(ifelse(is.na(protein$caption), NA, protein$caption))
     }
   }, error = function(e)  {
-    Sys.sleep(2) #if error, sleep 2 sec, then redo
+    Sys.sleep(4) #if error, sleep 2 sec, then redo
     protein_elink <- rentrez::entrez_link(dbfrom = "nuccore", id = transcript, db = "protein")
     protein_id <- protein_elink$links$nuccore_protein
     protein <- rentrez::entrez_summary(db = "protein", id = protein_id)
--- inst/doc/refseqR.R
+++ inst/doc/refseqR.R
@@ -17,28 +17,29 @@ GeneID <- c("LOC105852298")
 transcript <- refseq_fromGene(GeneID, sequence = "transcript")
 protein    <- refseq_fromGene(GeneID, sequence = "protein")
 
-## -----------------------------------------------------------------------------
-id <- c("LOC101512347")
-refseq_description(id)
+## ---- eval=FALSE--------------------------------------------------------------
+#  id <- c("LOC101512347")
+#  refseq_description(id)
 
-## -----------------------------------------------------------------------------
-mrna_gb <- rentrez::entrez_fetch(db= 'nuccore', id = "XM_004487701", rettype = 'gp') 
-strsplit(mrna_gb, "\n")[[1]][1:30]
+## ----eval=FALSE---------------------------------------------------------------
+#  mrna_gb <- rentrez::entrez_fetch(db= 'nuccore', id = "XM_004487701", rettype = 'gp')
+#  strsplit(mrna_gb, "\n")[[1]][1:30]
 
 ## ---- eval = F----------------------------------------------------------------
 #  transcript = c("XM_004487701", "XM_004488493", "XM_004501904")
 #  feat = c("caption", "moltype", "sourcedb", "slen", "title")
 #  refseq_mRNAfeat(transcript, feat)
 
-## -----------------------------------------------------------------------------
-transcript <- "XM_004487701"
-refseq_RNA2protein(transcript)
+## ---- eval=FALSE--------------------------------------------------------------
+#  transcript <- "XM_004487701"
+#  refseq_RNA2protein(transcript)
 
 ## -----------------------------------------------------------------------------
 refseq_CDScoords(transcript)
 refseq_CDSseq(transcript)
 
 ## -----------------------------------------------------------------------------
+transcript <- "XM_004487701"
 mrna_fasta = rentrez::entrez_fetch(db="nuccore", id=transcript, rettype="fasta")
 # take a look at the first 500 chars. 
 cat(strwrap(substr(mrna_fasta, 1, 500)), sep="\n")
@@ -46,22 +47,22 @@ cat(strwrap(substr(mrna_fasta, 1, 500)), sep="\n")
 ## -----------------------------------------------------------------------------
 substr(toString(refseq_CDSseq(transcript)), 1, 60)
 
-## -----------------------------------------------------------------------------
-id <- "XM_004487701"
-refseq_description(id)
+## ---- eval=FALSE--------------------------------------------------------------
+#  id <- "XM_004487701"
+#  refseq_description(id)
 
-## -----------------------------------------------------------------------------
-protein <- "XP_020244413"
-refseq_protein2RNA(protein)
+## ---- eval = FALSE------------------------------------------------------------
+#  protein <- "XP_020244413"
+#  refseq_protein2RNA(protein)
 
-## -----------------------------------------------------------------------------
-refseq_AAlen(protein)
-refseq_AAmol_wt(protein)
+## ---- eval=FALSE--------------------------------------------------------------
+#  refseq_AAlen(protein)
+#  refseq_AAmol_wt(protein)
 
 ## -----------------------------------------------------------------------------
 refseq_AAseq(protein)
 
-## -----------------------------------------------------------------------------
-id <- "XP_020244413"
-refseq_description(id)
+## ---- eval=FALSE--------------------------------------------------------------
+#  id <- "XP_020244413"
+#  refseq_description(id)
 
